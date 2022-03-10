6a75ac32013a14bcedde17d3cf896c798ecb2dcc
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2022-03-10 02:33:55 +0200

    Add files via upload

---
+++ third_task/second/main.js
@@ -0,0 +1,14 @@
+// moha
+
+/*
+
+*/
+
+document.write("<h1> hello world </h1>");
+console.log("hellow ");
+console.log("%c Elzero", "color: Red; font-size: 40px");
+
+console.log(
+  "%c Elzero",
+  "color: blue; font-size: 80px ; font-weight: bold ; text-align : center ;font-family: Arial"
+);
