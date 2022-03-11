e16aadd08b306e183b0fe33b6cd3638a7c7fc9ab
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2022-03-11 23:27:32 +0200

    Add files via upload

---
+++ third task/the third/main.js
@@ -0,0 +1,36 @@
+// 3 questions:
+
+// the first:
+console.log(10 * 20 + (15 % 3) + 190 + 10 - 400); // 0
+
+// the second:
+
+var num = 3;
+
+// Solution One
+console.log(num * 2); // 6
+
+// Solution Two
+console.log(num ** 2 - num); // 6
+
+// Soultion Three
+console.log(num + num); // 6
+
+// Soultion Four
+console.log((4 * num ** 2) / 6); // 6
+
+// the third
+
+var num = "10";
+
+// Solution One
+console.log(num * 2); // 20
+
+// Solution Two
+console.log(num ** 2 / 5); // 20
+
+// Solution Three
+console.log(num * 3 - num); // 20
+
+// Solution Four
+console.log(num * 4 - 2 * num); // 20
