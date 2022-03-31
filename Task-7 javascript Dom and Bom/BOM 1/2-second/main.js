51ccc40243132ce542a658a87cfa200588a30614
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2022-04-01 00:22:57 +0200

    Update main.js

--- Task-7 javascript Dom and Bom/BOM 1/2-second/main.js
+++ Task-7 javascript Dom and Bom/BOM 1/2-second/main.js
@@ -1,3 +1,29 @@
 const openmodelbutton = document.querySelectorAll("[data-model-target]");
 const closemodelbutton = document.querySelectorAll("[data-close-model]");
 const overlay = document.getElementById("overlay");
+
+openmodelbutton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+closemodelbutton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classlist.add("active");
+  overlay.classlist.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classlist.remove("active");
+  overlay.classlist.remove("active");
+}
